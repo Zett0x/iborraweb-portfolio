1b7237276b0d3a96bc4cfa27898bb69fe13b36e3
Zett0x <zetto1514@gmail.com>
2022-03-07 10:10:26 +0100

    about section responsive

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -7,41 +7,39 @@ import github from "../../assets/github-logo.png";
 
 export const Header = () => {
   return (
-    <>
-      <div id="app__header">
-        <Navbar />
-        <div className="app__header-content">
-          <div className="app__header-content-title">
-            <h1>👋 I am Francisco Iborra</h1>
-            <p>Front-end Web Developer</p>
+    <div id="app__header">
+      <Navbar />
+      <div className="app__header-content">
+        <div className="app__header-content-title">
+          <h1>👋 I am Francisco Iborra</h1>
+          <p>Front-end Web Developer</p>
+        </div>
+        <div className="app__header-content-media">
+          <div className="header-img">
+            <img src={photo} alt="francisco iborra" />
           </div>
-          <div className="app__header-content-media">
-            <div className="header-img">
-              <img src={photo} alt="francisco iborra" />
-            </div>
-            <div className="header-social-media">
-              <a
-                href="https://www.linkedin.com/in/francisco-javier-iborra-castro-4311691a4/"
-                // target="_blank"
-                // rel="noreferrer"
-              >
-                <img
-                  className="linkedin-logo"
-                  src={linkedin}
-                  alt="linkedin-logo"
-                />
-              </a>
-              <a
-                href="https://github.com/zett0x"
-                // target="_blank"
-                // rel="noreferrer"
-              >
-                <img className="github-logo" src={github} alt="github-logo" />
-              </a>
-            </div>
+          <div className="header-social-media">
+            <a
+              href="https://www.linkedin.com/in/francisco-javier-iborra-castro-4311691a4/"
+              // target="_blank"
+              // rel="noreferrer"
+            >
+              <img
+                className="linkedin-logo"
+                src={linkedin}
+                alt="linkedin-logo"
+              />
+            </a>
+            <a
+              href="https://github.com/zett0x"
+              // target="_blank"
+              // rel="noreferrer"
+            >
+              <img className="github-logo" src={github} alt="github-logo" />
+            </a>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
